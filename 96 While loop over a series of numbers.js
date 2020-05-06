5c03f4599d16b540a59d0f7a8040a749ac766ed7
Isaac Kendall <kendall.isaac@gmail.com>
2020-05-05 20:28:50 -0700

    initial commit

---
+++ 96 While loop over a series of numbers.js
@@ -0,0 +1,20 @@
+function loopASequence(start, end) {
+  // create a loop which runs if start is less than or equal to end
+    // log current value of start to console
+    // increment value of start
+}
+
+loopASequence(2, 5);
+// console output:
+  // 2
+  // 3
+  // 4
+  // 5
+
+loopASequence(3, 7);
+// console output:
+  // 3
+  // 4
+  // 5
+  // 6
+  // 7
