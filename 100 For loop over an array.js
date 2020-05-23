c84466ef87f4048073a651e6bd75cb7e8644005d
Isaac Kendall <kendall.isaac@gmail.com>
2020-05-23 15:44:16 -0700

    solved!

---
+++ 100 For loop over an array.js
@@ -0,0 +1,25 @@
+function loopAnArrayAgain(array) {
+    // create a loop which iterates over the input array
+      // log current array element to the console
+
+      for (var i = 0; i < array.length; i++) {
+          console.log(array[i]);
+          
+      }
+  }
+
+
+loopAnArrayAgain(['a', 'b', 'c', 'd']);
+// console output:
+  // a
+  // b
+  // c
+  // d
+
+loopAnArrayAgain([1, 2, 3, 4, 5]);
+// console output:
+  // 1
+  // 2
+  // 3
+  // 4
+  // 5
