a3ed9322e376290b1f2b0203a116755423065d7c
Isaac Kendall <kendall.isaac@gmail.com>
2020-05-23 15:54:23 -0700

    solved

---
+++ 99 For loop over a series of numbers.js
@@ -0,0 +1,25 @@
+function loopASequenceAgain(start, end) {
+    // create a loop which loops from start to end
+      // log current value to console
+
+      for (var i = start; i <= end; i++) {
+          console.log(i);
+          
+      }
+  
+  }
+
+loopASequenceAgain(2, 5);
+// console output:
+  // 2
+  // 3
+  // 4
+  // 5
+
+loopASequenceAgain(3, 7);
+  // console output:
+    // 3
+    // 4
+    // 5
+    // 6
+    // 7
