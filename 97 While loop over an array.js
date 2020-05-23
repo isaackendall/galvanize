59bf2bf462575feb4206ef2d4f6ff705a8e60e3f
Isaac Kendall <kendall.isaac@gmail.com>
2020-05-23 15:21:18 -0700

    Add files via upload
    solved!

---
+++ 97 While loop over an array.js
@@ -0,0 +1,31 @@
+function loopAnArray(array) {
+    // create an index variable
+    var index = 0;
+    // create a loop which iterates over the input array
+    while (index < array.length) {
+        // console.log(index);
+        console.log(array[index]);
+        index++;
+    }
+
+      // log current array element to the console
+      // increment value of index variable
+  }
+
+
+  
+
+  loopAnArray(['a', 'b', 'c', 'd']);
+// console output:
+  // a
+  // b
+  // c
+  // d
+
+loopAnArray([1, 2, 3, 4, 5]);
+// console output:
+  // 1
+  // 2
+  // 3
+  // 4
+  // 5
