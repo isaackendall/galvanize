1cdda32db036d26e9ecf1d46afc8e1d13cf05387
Isaac Kendall <kendall.isaac@gmail.com>
2024-12-12 20:18:49 +1300

    Create computeTripledAreaOfARectangle.js
    solved

---
+++ Math 3/computeTripledAreaOfARectangle.js
@@ -0,0 +1,14 @@
+function computeTripledAreaOfARectangle(length, width) {
+    const area = length * width;
+    return area * 3;
+  }
+
+  //computePerimeterOfACircle
+
+  function computePerimeterOfACircle(radius) {
+   return 2 * Math.PI * radius
+  }
+
+  function computeAreaOfACircle(radius) {
+    return Math.PI * (radius ** 2);
+  }
